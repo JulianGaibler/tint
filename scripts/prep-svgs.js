8d6a646820fda3deadde0fadaa64c703456a07de
Juliana Gaibler <git@juliana.me>
2024-02-02 02:49:44 +0100

    :recycle: Simplified icon prep script

--- scripts/prep-svgs.js
+++ scripts/prep-svgs.js
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import { EOL } from 'os'
 import path from 'path'
 import { optimize } from 'svgo'
 import util from 'util'
@@ -11,66 +12,31 @@ const processSVG = async (filePath) => {
   try {
     const content = await readFileAsync(filePath, 'utf-8')
 
-    // Remove <defs> tag and its content
-    let processedContent = content.replace(/<defs>[\s\S]*<\/defs>/g, '')
-
-    // Remove specific attributes (id, data-name, class, fill, stroke)
-    processedContent = processedContent.replace(
-      /(?:id|data-name|class|fill|stroke)\s*=\s*['"][^'"]*['"]/g,
-      '',
-    )
-
-    // Add fill="currentColor" attribute
-    processedContent = processedContent.replace(
-      /<svg/g,
-      '<svg fill="currentColor"',
-    )
-
-    // If there is a <title> tag, remove it
-    processedContent = processedContent.replace(/<title>[\s\S]*<\/title>/g, '')
-
-    // if there is a viewbox but no width/height, add width/height with the same value as viewbox
-    if (
-      processedContent.match(/viewBox="[\s\S]*"/g) &&
-      !processedContent.match(/width="[\s\S]*"/g)
-    ) {
-      const { width, height } = extractViewBoxValues(processedContent)
-      processedContent = appendAttributesToTags(
-        processedContent,
-        'svg',
-        `width="${width}" height="${height}"`,
-      )
-    } else if (
-      // if there is a width/height but no viewbox, add viewbox with the same value as width/height
-      processedContent.match(/width="[\s\S]*"/g) &&
-      !processedContent.match(/viewBox="[\s\S]*"/g)
-    ) {
-      // extract the width/height values (e.g. width="24" height="20")
-      const width = processedContent.match(/width="([\s\S]*)"/)[1]
-      const height = processedContent.match(/height="([\s\S]*)"/)[1]
-      processedContent = appendAttributesToTags(
-        processedContent,
-        'svg',
-        `viewBox="0 0 ${width} ${height}"`,
-      )
-    }
-
     // Run svgo
-    const result = await optimize(processedContent, {
+    const result = optimize(content, {
       plugins: [
-        {
-          name: 'preset-default',
-          params: {
-            overrides: {
-              removeViewBox: false,
-            },
-          },
-        },
+        svgoRemoveAttrs([
+          'id',
+          'data-name',
+          'class',
+          'fill',
+          'stroke',
+          'stroke-width',
+          'stroke-miterlimit',
+          'clip-rule',
+          'fill-rule',
+          'fill-opacity',
+        ]),
+        viewBoxAndDimensions,
+        addContextFill,
+        ...svgoBasePlugins,
       ],
     })
 
+    console.log(result)
+
     // Write the optimized SVG back to the file
-    await writeFileAsync(filePath, result.data, 'utf-8')
+    await writeFileAsync(filePath, `${result.data}${EOL}`, 'utf-8')
 
     console.log(`Processed: ${filePath}`)
   } catch (error) {
@@ -101,26 +67,70 @@ const processSVGs = async (dir) => {
 // Run the script
 processSVGs('src/lib/icons')
 
-function appendAttributesToTags(htmlString, tagName, attributesToAdd) {
-  const regex = new RegExp(`(<${tagName}\\s*[^>]*>)`, 'gi')
-
-  htmlString = htmlString.replace(regex, (_, openingTag) => {
-    const updatedTag = openingTag.replace('>', ` ${attributesToAdd}>`)
-    return updatedTag
-  })
-
-  return htmlString
+const viewBoxAndDimensions = {
+  name: 'viewBoxAndDimensions',
+  fn: () => ({
+    element: {
+      enter(node) {
+        if (node.name !== 'svg') {
+          return
+        }
+        const viewBox = node.attributes.viewBox
+        const width = node.attributes.width
+        const height = node.attributes.height
+        if (viewBox && width && height) {
+          return
+        }
+        if ((viewBox && !width) || !height) {
+          const [, , w, h] = viewBox.split(' ')
+          node.attributes.width = w
+          node.attributes.height = h
+        } else if (width && height && !viewBox) {
+          node.attributes.viewBox = `0 0 ${width} ${height}`
+        } else {
+          throw new Error('SVG has no width, height, or viewBox')
+        }
+      },
+    },
+  }),
 }
 
-function extractViewBoxValues(svgString) {
-  const viewBoxRegex =
-    /viewBox\s*=\s*["']\s*([^"'\s]+)\s+([^"'\s]+)\s+([^"'\s]+)\s+([^"'\s]+)\s*["']/i
-  const match = svgString.match(viewBoxRegex)
+const addContextFill = {
+  name: 'addContextFill',
+  fn: () => ({
+    element: {
+      enter(node) {
+        if (node.name !== 'svg') {
+          return
+        }
+        node.attributes.fill = 'currentColor'
+      },
+    },
+  }),
+}
 
-  if (match) {
-    const [, minX, minY, width, height] = match
-    return { minX, minY, width, height }
-  } else {
-    return null // If viewBox attribute is not found
+const svgoBasePlugins = [
+  'removeDesc',
+  'removeStyleElement',
+  'removeOffCanvasPaths',
+  'removeNonInheritableGroupAttrs',
+  'sortAttrs',
+  {
+    name: 'preset-default',
+    params: {
+      overrides: {
+        removeViewBox: false,
+      },
+    },
+  },
+]
+
+function svgoRemoveAttrs(attrs) {
+  const attrString = attrs.map((attr) => attr.trim()).join('|')
+  return {
+    name: 'removeAttrs',
+    params: {
+      attrs: `(${attrString})`,
+    },
   }
 }
