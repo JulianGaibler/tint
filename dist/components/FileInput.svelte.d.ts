5743c2a7b8f2d84641c504c716cc453c6c6de870
JulianGaibler <julian@jwels.berlin>
2024-02-03 21:09:58 +0100

    :recycle: Renamed FileUpload to FileInput

--- dist/components/FileInput.svelte.d.ts
+++ dist/components/FileInput.svelte.d.ts
@@ -17,9 +17,9 @@ declare const __propDef: {
     };
     slots: {};
 };
-export type FileUploadProps = typeof __propDef.props;
-export type FileUploadEvents = typeof __propDef.events;
-export type FileUploadSlots = typeof __propDef.slots;
-export default class FileUpload extends SvelteComponent<FileUploadProps, FileUploadEvents, FileUploadSlots> {
+export type FileInputProps = typeof __propDef.props;
+export type FileInputEvents = typeof __propDef.events;
+export type FileInputSlots = typeof __propDef.slots;
+export default class FileInput extends SvelteComponent<FileInputProps, FileInputEvents, FileInputSlots> {
 }
 export {};
